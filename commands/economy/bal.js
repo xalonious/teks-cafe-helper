a4273e6fe6d6275531c1169aa5f4be8c323b70d1
xalonious <xander.lauwens@gmail.com>
2024-04-30 17:31:58 +0200

    Bug fix in bal command

--- commands/economy/bal.js
+++ commands/economy/bal.js
@@ -27,7 +27,6 @@ module.exports = {
         const balEmbed = new EmbedBuilder()
         .setTitle(`${user.username}'s balance`)
         .setDescription(`<a:tekcoin:1234188584664436778> | Balance: ${existingUser.balance} \n <a:tekcoin:1234188584664436778> | Bank Balance: ${existingUser.bankbalance}/${existingUser.bankCapacity}`)
-        .addField("<a:tekcoin:1234188584664436778> | Bank Balance", `${existingUser.amountInBank}/${existingUser.bankCapacity}`)
         .setColor("Random")
         .setThumbnail(user.displayAvatarURL(), { dynamic: true })
         .setFooter({ text: "you poor lmao"})
