b46543d3a72e90c3f0a80d56b50928c197191353
xalonious <xander.lauwens@gmail.com>
2024-05-01 01:34:50 +0200

    I hate working with dates

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -1,4 +1,5 @@
 const userAccount = require("../../schemas/userAccount");
+const ms = require("ms"); 
 
 module.exports = {
     name: "daily",
@@ -6,35 +7,18 @@ module.exports = {
     requiresAccount: true,
 
     run: async (client, interaction) => {
-
-        await interaction.deferReply();
-
         const user = await userAccount.findOne({ userId: interaction.user.id });
 
-        const now = new Date();
-
-        const midnightUTC = new Date(
-            now.getUTCFullYear(),
-            now.getUTCMonth(),
-            now.getUTCDate() + 1, 
-            0,  
-            0, 
-            0,
-            0 
-        );
-
-        const timeLeft = midnightUTC - now; 
-
-        const hoursLeft = Math.floor(timeLeft / (1000 * 60 * 60));
-        const minutesLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-
         if (user.hasClaimedDaily) {
-            return interaction.editReply(`you already claimed your daily coins today, stop being so damn greedy... you can claim them again in **${hoursLeft} hours and ${minutesLeft} minutes**`);
+            return interaction.reply(`you already claimed your daily coins today, stop being so damn greedy... you can claim them at midnight UTC`);
         }
 
         user.walletbalance += 2500;
         user.hasClaimedDaily = true;
+
+
         await user.save();
+
 
         await interaction.editReply(`<a:tekcoin:1234188584664436778> You have claimed your daily **__2500 coins__**!`);
     }
