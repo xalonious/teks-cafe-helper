ae298c826d837310f958c951ff5a212c034c5b02
xalonious <74146476+xalonious@users.noreply.github.com>
2024-05-02 12:15:26 +0200

    Update deleteAccount.js

--- events/guildMemberRemove/deleteAccount.js
+++ events/guildMemberRemove/deleteAccount.js
@@ -3,5 +3,5 @@ const userAccount = require("../../schemas/userAccount");
 module.exports = async (client, guildMember) => {
     const user = await userAccount.findOne({ userId : guildMember.id });
     if (!user) return;
-    await user.delete();
+    await user.deleteOne();
 }
